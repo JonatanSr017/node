98a7c99958372a8012ed526ac53225246cfb00ba
jonatanSr017 <jonatansr017@gmail.com>
2025-03-14 14:33:23 -0300

    inicio

---
+++ aula1.js
@@ -0,0 +1,27 @@
+var nome = "Jonatan"; //string -> texto
+var idade = 64; //number -> int
+var peso = 23.4; // number -> float
+var andando = true; // boolean
+
+// == igual
+// != diferente
+// > maior
+// < menor
+// >= maior ou igual
+// >= menor ou igual
+
+if (idade >= 64)
+{
+    console.log("Idoso");
+} else if (idade >= 18)
+
+{
+    console.log("Adulto");
+} else if (idade >= 12)
+    
+{
+    console.log("Adolescente");
+} else
+{
+    console.log("Criança");
+}
---
+++ aula2.js
@@ -0,0 +1,25 @@
+let aluno = "Jonatan";
+
+// estrutura de dados
+// arrey -> vetores
+
+let alunos = ["Pedro", "Ana", "João", 123];
+
+alunos.push("Mario");
+
+alunos[3] = "Maria";
+
+// loops -> repetições
+
+//let ini = alunos.length -1;
+//let fim = 0;
+
+//while (fim <= ini)
+//{
+//    console.log(alunos[ini] );
+//    ini = ini - 1;
+//}
+
+// ordenação
+
+console.log(alunos.sort());
